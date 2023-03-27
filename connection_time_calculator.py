2a1f198fe3226e8e56b4acf4f39c29f012253769
Harry980806 <104696256+Harry980806@users.noreply.github.com>
2023-03-27 10:52:25 -0700

    Update connection_time_calculator.py

--- connection_time_calculator.py
+++ connection_time_calculator.py
@@ -193,7 +193,7 @@ def main():
                     st.markdown('The flight time for second flight is ' + str(round(final['Total Blk time_f2'][0].hour + 
                                                                                     final['Total Blk time_f2'][0].minute/60,1)) + ' hours')
                     st.markdown('The total travel time is' + 
-                                str(round(final['Total Blk time_f1'][0].hour+ final['Total Blk time_f1'][0].minute/60
+                                str(round(final['Total Blk time_f1'][0].hour+ final['Total Blk time_f1'][0].minute/60+
                                       final['connection time'][0].days * 24 + 
                                       final['connection time'][0].seconds/3600 +
                                       final['Total Blk time_f2'][0].hour + final['Total Blk time_f2'][0].minute/60,1)) +' hours')
