794d5009a3fe12b76d0f75c21eceb3324f71263c
Harry980806 <104696256+Harry980806@users.noreply.github.com>
2023-03-27 14:42:03 -0700

    Update connection_time_calculator.py

--- connection_time_calculator.py
+++ connection_time_calculator.py
@@ -74,10 +74,10 @@ def main():
     
     st.title('Flight Route & Time Calculator ✈️ ')
     
-    st.write('<p style="font-size:14px; color:Black;">Given the AWB origin,destination,aircraft type and desired first flight date, 
-             this app will propose a flight route to minimize the total travel time, which is defined by flight time + connection time at the transition airport.</p>',
-                    unsafe_allow_html=True)
-
+    st.write('<p style="font-size:14px; color:Black;">Given the AWB origin,destination,aircraft type and desired first flight date.</p>',
+                 unsafe_allow_html=True)
+    st.write('<p style="font-size:14px; color:Black;">this app will propose a flight route to minimize the total travel time, which is defined by flight time + connection time at the transition airport.</p>',
+                 unsafe_allow_html=True)
 
     pax= load_data_excel('S23_WB_NB_DH4_08Mar-28Oct.xlsx','S23 Pax Leg Sked')
 
