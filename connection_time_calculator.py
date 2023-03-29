1b8f0c1c5c54efe93409c819b46228c64493b70c
Harry980806 <104696256+Harry980806@users.noreply.github.com>
2023-03-29 15:26:44 -0700

    Update connection_time_calculator.py

--- connection_time_calculator.py
+++ connection_time_calculator.py
@@ -317,7 +317,7 @@ def main():
                     st.table(result_df1.T)
                     
                     st.markdown('The connection time at {} is: '.format(final['Dept Sta_f2'][0])+
-                                 str(final['connection time'][0]))
+                                 str(round(final['connection time'][0],1)) +' hours')
                         
 #                     st.markdown('The connection time at {} is: '.format(final['Dept Sta_f2'][0])+
 #                                 str(round(final['connection time'][0].days * 24 + final['connection time'][0].seconds/3600,1)) + ' hours')
