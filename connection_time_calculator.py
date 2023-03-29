7b2f040efa6dbe15ca3ed12eec4193ecfed75656
Harry980806 <104696256+Harry980806@users.noreply.github.com>
2023-03-29 15:35:38 -0700

    Update connection_time_calculator.py

--- connection_time_calculator.py
+++ connection_time_calculator.py
@@ -71,6 +71,15 @@ def data_cleaning(df1,df2,df3):
 
 
 def main():
+     title_container = st.beta_container()
+        col1, col2 = st.beta_columns([1, 20])
+        image = Image.open('/home/ddutt/Pictures/Suzieq-logo-2.jpg')
+        with title_container:
+            with col1:
+                st.image(image, width=64)
+            with col2:
+                st.markdown('<h1 style="color: purple;">Suzieq</h1>',
+                            unsafe_allow_html=True)
     
     st.title('WestJet Cargo Flight Scanner ✈️')
     
